9dd9bf1da92b920117e173f33f53da58e32c2ba7
Jonikorjk <zenaserdykov2004@gmail.com>
2024-11-24 12:35:32 +0200

    Add doc

--- Sources/ed25519/Core/Point.swift
+++ Sources/ed25519/Core/Point.swift
@@ -8,9 +8,12 @@
 import Clibsodium
 
 extension Ed25519.CoreWrapper.Core {
+    /// Q = G * k
+    ///
     /// Calculates the point without clamping the scalar.
+    ///
     /// # Parameters
-    /// - scalar - scalar bytes in the big-endian order.
+    /// - scalar - scalar bytes in the little-endian order.
     /// # Output
     /// Returns the point bytes in little-endian order.
     static func basePointMulScalarNoclamp(_ scalar: [UInt8]) throws -> [UInt8] {
@@ -31,6 +34,14 @@ extension Ed25519.CoreWrapper.Core {
         return q
     }
 
+    /// Q = G * k
+    ///
+    /// Calculates the point with clamping the scalar.
+    ///
+    /// # Parameters
+    /// - scalar - scalar bytes in the little-endian order.
+    /// # Output
+    /// Returns the point bytes in little-endian order.
     static func basePointMulScalar(_ scalar: [UInt8]) throws -> [UInt8] {
         var scalar = scalar
         padEndZerosIfNeeded(elem: &scalar)
@@ -50,6 +61,16 @@ extension Ed25519.CoreWrapper.Core {
 
     }
 
+    /// R = P + Q
+    ///
+    /// Adds two points.
+    ///
+    /// # Parameters
+    /// - p - little-endian bytes point representation
+    /// - q - little-endian bytes point representation
+    ///
+    /// # Output
+    /// Returns the point bytes in little-endian order.
     static func addPoints(_ p: [UInt8], _ q: [UInt8]) throws -> [UInt8] {
         var p = p
         var q = q
@@ -75,6 +96,17 @@ extension Ed25519.CoreWrapper.Core {
         return r
     }
 
+
+    /// R = P - Q
+    ///
+    /// Subtracts two points.
+    ///
+    /// # Parameters
+    /// - p - little-endian bytes point representation
+    /// - q - little-endian bytes point representation
+    ///
+    /// # Output
+    /// Returns the point bytes in little-endian order.
     static func subPoints(_ p: [UInt8], _ q: [UInt8]) throws -> [UInt8] {
         var p = p
         var q = q
@@ -86,7 +118,7 @@ extension Ed25519.CoreWrapper.Core {
             throw Ed25519WrapperError.invalidPointLenth(length: [p.count, q.count])
         }
 
-        var r = [UInt8].init(repeating: 0, count: 32)
+        var r = [UInt8](repeating: 0, count: 32)
         crypto_core_ed25519_sub(&r, p, q)
 
         if r.count != 32 {
@@ -100,6 +132,7 @@ extension Ed25519.CoreWrapper.Core {
         return r
     }
 
+    /// Checks point is valid ed25519 point. 
     static func isValidPoint(_ p: [UInt8]) throws -> Bool {
         var p = p
 
--- Sources/ed25519/Core/Scalar.swift
+++ Sources/ed25519/Core/Scalar.swift
@@ -8,6 +8,16 @@
 import Clibsodium
 
 extension Ed25519.CoreWrapper.Core {
+    /// a - b = c
+    ///
+    /// Subtracts two scalars
+    ///
+    /// # Parameters
+    /// - x - little-endian bytes scalar representation
+    /// - y - little-endian bytes scalar representation
+    ///
+    /// # Output
+    /// Returns little-endian scalar bytes
     static func scalarSub(_ x: [UInt8], _ y: [UInt8]) -> [UInt8] {
         var x = x
         var y = y
@@ -21,6 +31,16 @@ extension Ed25519.CoreWrapper.Core {
         return Array(z.reversed())
     }
 
+    /// a + b = c
+    ///
+    /// Adds two scalars
+    ///
+    /// # Parameters
+    /// - x - little-endian bytes scalar representation
+    /// - y - little-endian bytes scalar representation
+    ///
+    /// # Output
+    /// Returns little-endian scalar bytes
     static func scalarAdd(_ x: [UInt8], _ y: [UInt8]) -> [UInt8] {
         var x = x
         var y = y
@@ -34,6 +54,7 @@ extension Ed25519.CoreWrapper.Core {
         return Array(z.reversed())
     }
 
+    /// Returns random little-endian scalar bytes
     static func scalarRandom() -> [UInt8] {
         var bytes = [UInt8](repeating: 0, count: 32)
         crypto_core_ed25519_scalar_random(&bytes)
